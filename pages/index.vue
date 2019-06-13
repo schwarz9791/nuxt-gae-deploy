a2fee1d7fa0bbdd74ebac9ac0cbe7e1907b7958f
Masaki Matsumura <mm.schwarz@gmail.com>
2019-06-13 12:02:18 +0900

    TEST: ci deploy after add firebase

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,8 @@
       </h1>
       <h2 class="subtitle">
         My impeccable Nuxt.js project
+        <br>
+        Add firebase.
       </h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green"
